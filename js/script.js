1c7421bc5f659d33d0710a75a34f07dab06aaffc
ManfredAtGit <manfred.at.home@gmail.com>
2016-09-28 13:06:24 +0200

    styles changed for container, gallery added to main menue, scprit added to collapse menue

--- js/script.js
+++ js/script.js
@@ -1,26 +1,3 @@
-/*
-$(function () { // Same as document.addEventListener("DOMContentLoaded"...
-
-  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
-  $("#navbarToggle").blur(function (event) {
-    var screenWidth = window.innerWidth;
-    if (screenWidth < 768) {
-      $("#myNavbar").collapse('hide');
-    }
-  });
-
-  // In Firefox and Safari, the click event doesn't retain the focus
-  // on the clicked button. Therefore, the blur event will not fire on
-  // user clicking somewhere else in the page and the blur event handler
-  // which is set up above will not be called.
-  // Refer to issue #28 in the repo.
-  // Solution: force focus on the element that the click event fired on
-  //$("#navbarToggle").click(function (event) {
-  //  $(event.target).focus();
-  //});
-});
-
-*/
 
 // global var for storing choosen language
 $lang = "de";
@@ -42,6 +19,26 @@ $(document).ready(function(){
      $("#en_flag").toggle();
      $lang ="en";
      $dc.loadHome();
+  });
+
+  /*
+  To collapse menue when user clicks menue item or clicks somewhere else
+  */
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbarToggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#myNavbar").collapse('hide');
+    }
+  });
+  // In Firefox and Safari, the click event doesn't retain the focus
+  // on the clicked button. Therefore, the blur event will not fire on
+  // user clicking somewhere else in the page and the blur event handler
+  // which is set up above will not be called.
+  // Refer to issue #28 in the repo.
+  // Solution: force focus on the element that the click event fired on
+  $("#navbarToggle").click(function (event) {
+    $(event.target).focus();
   });
 
 });
